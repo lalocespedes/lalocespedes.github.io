6fc68859b3268472eb5905ac1218eeffc78a5cd4
Eduardo Cespedes <lalocespedes@gmail.com>
2017-01-16 14:56:22 -0600

    cambios

--- js/src/main/controllers/mainController.js
+++ js/src/main/controllers/mainController.js
@@ -5,40 +5,40 @@
         .module('lalocespedes')
         .controller('MainController', MainController);
 
-    MainController.inject = ['$log', '$window', 'FileSaver', 'Blob'];
-    function MainController($log, $window, FileSaver, Blob) {
+    MainController.inject = ['$log', '$window', 'FileSaver', 'Blob', '$http'];
+    function MainController($log, $window, FileSaver, Blob, $http) {
         var vm = this;
 
-        $log.info("Wellcome XXI");
-
         activate();
+
+
 
         ////////////////
 
         function activate() {
 
             vm.gitclick = function (href) {
-
                 $window.open(href, 'popup', "width=800,height=600,left=10,top=150");
-
             };
 
             vm.print = function () {
-
                 print();
                 close();
-
             };
 
             vm.downloadpdf = function () {
 
-                $http.get('').then(function (response) {
-                    var blob = new Blob([data], { response: "application/pdf" });
+                $http.get('http://localhost:8000/lalocespedes.pdf',{
+                    responseType: 'arraybuffer'
+                }).then(function (response) {
+
+                    // $log.debug(response.data);
+
+                    var blob = new Blob([response.data], { response: "application/pdf" });
                     FileSaver.saveAs(blob, "lalocespedes.pdf");
                 });
 
             };
-
         }
     }
 
